07899688b1152f6d8a0d4264753d9e09c4f64211
flourishcodes <kunle@adeniran.com>
2018-07-23 16:43:39 -0700

    Test files

--- models/city.py
+++ models/city.py
@@ -3,11 +3,21 @@
     Define the class City.
 '''
 from models.base_model import BaseModel
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+    	name = Column(String(128), nullable=False)
+    	state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    
+    else:
+    	state_id = ""
+    	name = ""
--- models/state.py
+++ models/state.py
@@ -4,10 +4,30 @@
 '''
 
 from models.base_model import BaseModel
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from os import getenv
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    name = ""
+
+    __tablename__ = "states"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+    	name = Column(String(128), nullable=False)
+    	cities = relationship("City", backref="state")
+
+    else:
+    	name = ""
+   	@property
+    def cities(self):
+    	my_list = []
+    		
+    	for value in models.storage.all(City).values():
+    		if self.id == state_id:
+    			my_list.append(value)
+   		return my_list
+    	
