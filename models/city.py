3e1f9ace34f196039da67e1b2090aad8fc52029d
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-24 19:00:26 -0700

    Add places to ORM

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -16,7 +17,7 @@ class City(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
+        places = relationship('Place', cascade='all, delete', backref='city')
     else:
         state_id = ""
         name = ""
--- models/user.py
+++ models/user.py
@@ -5,18 +5,20 @@
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade='all, delete', backref='user')
     else:
         email = ""
         password = ""
