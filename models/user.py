3ced445e5e7809697d899d1e4a21181463e89eab
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-24 21:39:48 -0700

    Don't make a SQLAlchemy table in filestorage mode

--- models/user.py
+++ models/user.py
@@ -13,8 +13,8 @@ class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
