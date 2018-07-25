c314e15f029d024f8846b94efece9c64015f9b47
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-25 11:23:11 -0700

    Only create SQLA table in place.py if env flag is 'db' instead of exists

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-if getenv("HBNB_TYPE_STORAGE"):
+if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table("place_amenity", Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey("places.id"), primary_key=True),
