9bea3b3d8644633484223bf6f6b16934d0b99014
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-15 19:50:42 -0700

    Add files for flask route 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""The / route"""
+
+from models import storage
+from models.state import State
+
+
+if __name__ == "__main__":
+    from flask import Flask
+    import flask
+
+    app = Flask(__name__)
+
+    @app.route('/states_list/', strict_slashes=False)
+    def states_list():
+        """list states from database"""
+        states = storage.all(State)
+        stateidname = []
+        for state in states:
+            stateidname.append((states[state].id, states[state].name))
+        return flask.render_template('7-states_list.html',
+                                     stateidname=stateidname)
+
+    app.run(host='0.0.0.0')
